1319b2a1ca9426b4cac714bb8d93ed28f86a08b1
Kwablah <ernestkwablah19@gmail.com>
2025-07-27 17:12:05 +0000

    checking for error handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 try:
     mydb = mysql.connector.connect(
@@ -74,7 +73,7 @@ CREATE TABLE Orders (
     if mydb.is_connected():
         print("✅ Connected to the database successfully.")
 
-except Error as e:
+except mysql.connector.Error as e:
     print("Error:", e)
 
 if mycursor:
