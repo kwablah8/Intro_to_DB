89ab9547f84b5f932ec78f7ae4b54ad91418609d
Kwablah <ernestkwablah19@gmail.com>
2025-07-27 14:32:39 +0000

    mods 2

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,8 +24,8 @@ CREATE TABLE Customers (
 );
 
 CREATE TABLE Orders (
-    order_id PRIMARY KEY,
-    customer_id,
+    order_id INT PRIMARY KEY,
+    customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
