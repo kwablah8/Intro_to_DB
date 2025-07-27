edaa256a425036d71401e8a12e468e59a49160a8
Kwablah <ernestkwablah19@gmail.com>
2025-07-27 14:34:52 +0000

    mods 4

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,7 +33,7 @@ CREATE TABLE Orders (
 CREATE TABLE Order_Details (
     orderdetailid PRIMARY KEY,
     order_id,
-    book_id,
+    book_id INT,
     quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
